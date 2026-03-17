b54c5afacb4c0d8600fa5018a5ba4d4d5fd9c457
Lin Jian Wen <jianwen@bukku.my>
2026-03-18 01:35:40 +0800

    successfully spin up docker

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: 'standalone'
+  output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname)
+    };
+    return config;
+  }
 };
+
 module.exports = nextConfig;
